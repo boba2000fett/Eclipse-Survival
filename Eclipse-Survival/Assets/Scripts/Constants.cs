af71d3722bca5f9de550e6ebe4d8667ab2f17d28
Homerun1500 <cabrams1@email.davenport.edu>
2022-02-08 19:40:30 -0500

    Added constant script and TestFoodItem
    Added the shell scripts for the game constants and game manager. Also generated a FoodItemTest prefab for testing the basic item collection script

---
+++ Eclipse-Survival/Assets/Scripts/Constants.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+
+    //Constant Values for the Day/Night light cycle script
+    const float MAX_LIGHT_INTENSITY = 0.6f;
+    const float MIN_LIGHT_INTENSITY = 0.05f;
+    const float LIGHT_DECREASE_DURATION = 450f; // in seconds
+    const float LIGHT_INCREASE_DURATION = 450f; // in seconds
+    const float DAY_NIGHT_CUTOFF = 0.3f; // light intensity at which IsDaytime is toggled
+
+    //Constant Int Value for the Scene Index Value
+    const float SPLASH_SCENE = 0;
+    const float STUDIO_SCENE = 1;
+    const float MAIN_MENU_SCENE = 2;
+    const float PLAY_SCENE_TOP_DOWN_VIEW = 3;
+    const float PLAY_SCENE_WALL_VIEW = 4;
+    const float PAUSE_SCENE = 5;
+    const float GAME_OVER_SCENE = 6;
+    const float OPTION_SCENE = 7;
+    const float CREDIT_SCENE = 8;
+
+    //Constant Values for the game object variables
+
+}
---
+++ Eclipse-Survival/Assets/Scripts/GamePlayManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayManager : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject Xander;
+
+    [Header("Set Dynamically for testing")]
+    public int counter;
+
+    void Update()
+    {
+        
+    }
+}
